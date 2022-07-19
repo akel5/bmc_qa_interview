f8af84b2e684859f9b253b8ea2f8fdcf01347aeb
akel5 <akel.obied@outlook.com>
2022-07-19 11:39:10 +0300

    Adding Jsch dependency

--- src/main/java/com/akel/Main.java
+++ src/main/java/com/akel/Main.java
@@ -1,33 +1,56 @@
 package com.akel;
 
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.channel.ClientChannelEvent;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.common.channel.Channel;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /*JSch jsch = new JSch();
-        Session session=jsch.getSession("Akel-Laptop", "RemoteHostName", 8080);
-        session.setPassword("mindmind555");
-        Properties config = new Properties();
-        config.put("StrictHostKeyChecking", "no");
-        session.setConfig(config);
-        session.connect();
+        String command = "find /home/akel/test -printf \"%TY-%Tm-%Td\\t%s\\t%p\\n\" \n";
 
-        ChannelExec channel=(ChannelExec) session.openChannel("exec");
-        BufferedReader in=new BufferedReader(new InputStreamReader(channel.getInputStream()));
-        channel.setCommand("tree -f;");
-        channel.connect();
 
-        String msg=null;
-        while((msg=in.readLine())!=null){
-            System.out.println(msg);
+        SshClient client = SshClient.setUpDefaultClient();
+        client.start();
+
+        try (ClientSession session = client.connect("akel", "ec2-18-189-1-12.us-east-2.compute.amazonaws.com", 22)
+                .verify(30, TimeUnit.SECONDS).getSession()) {
+            session.addPasswordIdentity("akelakel");
+            session.auth().verify(30, TimeUnit.SECONDS);
+
+            try (ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+                 ClientChannel channel = session.createChannel(Channel.CHANNEL_SHELL)) {
+                channel.setOut(responseStream);
+                try {
+                    channel.open().verify(30, TimeUnit.SECONDS);
+                    try (OutputStream pipedIn = channel.getInvertedIn()) {
+                        pipedIn.write(command.getBytes());
+                        pipedIn.flush();
+                    }
+
+                    channel.waitFor(EnumSet.of(ClientChannelEvent.CLOSED),
+                            TimeUnit.SECONDS.toMillis(30));
+                    String responseString = responseStream.toString();
+
+                    System.out.println(responseString);
+
+                } finally {
+                    channel.close(false);
+                }
+            }
+        } finally {
+            //client.stop();
         }
-
-        channel.disconnect();
-        session.disconnect();
-
-         */
-        System.out.println("Hello world!");
     }
+
+
 }
